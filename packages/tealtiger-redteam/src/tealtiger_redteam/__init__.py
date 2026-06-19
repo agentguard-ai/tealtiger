a2e67d1d41ecefcaf8655ed9d06d7a6b1df54c41
CleanDev-Fix <219162456+CleanDev-Fix@users.noreply.github.com>
2026-06-19 09:43:24 -0400

    Add Haystack red-team scanner CLI

---
+++ packages/tealtiger-redteam/src/tealtiger_redteam/__init__.py
@@ -0,0 +1,5 @@
+"""Static red-team scanner for Haystack pipelines."""
+
+from tealtiger_redteam.scanner import Finding, ScanReport, scan_file, scan_source
+
+__all__ = ["Finding", "ScanReport", "scan_file", "scan_source"]
